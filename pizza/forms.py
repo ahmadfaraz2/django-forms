8124651188beecc829309d79083bcf592ea523d4
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-04-23 21:40:14 +0500

    Add widgets in forms

--- pizza/forms.py
+++ pizza/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Pizza
+from .models import Pizza, Size
 
 
 # class PizzaForm(forms.Form):
@@ -13,7 +13,12 @@ from .models import Pizza
 
 class PizzaForm(forms.ModelForm):
 
+    size = forms.ModelChoiceField(
+        queryset=Size.objects, empty_label=None, widget=forms.RadioSelect
+    )
+
     class Meta:
         model = Pizza
         fields = ["topping1", "topping2", "size"]
         labels = {"topping1": "Topping 1", "topping2": "Topping 2"}
+        # widgets = {"topping1": forms.Textarea}
