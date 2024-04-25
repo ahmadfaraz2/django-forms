fbc27e1845f0409ac0b5a486f7f644e6f4428c03
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-04-25 22:47:31 +0500

    Did styling with bootstrap and customiz forms with django widget tweaks

--- pizza/views.py
+++ pizza/views.py
@@ -21,17 +21,20 @@ def order(request):
                 filled_form.cleaned_data["topping1"],
                 filled_form.cleaned_data["topping2"],
             )
-            new_form = PizzaForm()
-            return render(
-                request,
-                "pizza/order.html",
-                {
-                    "created_pizza_pk": created_pizza_pk,
-                    "pizzaform": new_form,
-                    "note": note,
-                    "multiple_form": multiple_form,
-                },
-            )
+            filled_form = PizzaForm()
+        else:
+            created_pizza_pk = None
+            note = "Order has faild. Please try again!"
+        return render(
+            request,
+            "pizza/order.html",
+            {
+                "created_pizza_pk": created_pizza_pk,
+                "pizzaform": filled_form,
+                "note": note,
+                "multiple_form": multiple_form,
+            },
+        )
     else:
         form = PizzaForm()
         return render(
